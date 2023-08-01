080c60f8550350aa02b5d345bcbf4f1544a8e1ed
MarwanDev <marwaneg@aucegypt.edu>
2023-08-01 10:37:20 +0300

    Add table relationships

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
+  has_many :recipe_food, foreign_key: 'food_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-    has_many:recipe
+  has_many :recipe, foreign_key: 'user_id'
+  has_many :food, foreign_key: 'user_id'
 end
