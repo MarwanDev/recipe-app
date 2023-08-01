70f36d1f2ce49017f892517aa6059d22abf142f0
Joy Phoebe <joyapisi@gmail.com>
2023-08-01 10:53:45 +0300

    Add root route and extension for scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root "users#index"
   resources :recipes
   resources :recipe_foods
   resources :foods
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ("/") 
 end
