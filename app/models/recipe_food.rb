2356eb4783aa2eb85162f52ced80ba24c75940c0
Joy Phoebe <joyapisi@gmail.com>
2023-08-03 21:45:56 +0300

    Add validations to recipe_food model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,7 @@
 class RecipeFood < ApplicationRecord
   belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
   belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
+
+  validates :food, presence: true
+  validates :recipe, presence: true
 end
