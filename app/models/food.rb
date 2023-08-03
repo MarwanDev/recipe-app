35bb982c1cd2d39c464589bf751f7f14fb3610b1
Joy Phoebe <joyapisi@gmail.com>
2023-08-03 21:58:57 +0300

    Add validations for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,6 @@ class Food < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :recipe_foods, foreign_key: 'food_id'
   has_many :recipes, through: :recipe_foods, dependent: :destroy
+
+  validates :user, presence: true
 end
