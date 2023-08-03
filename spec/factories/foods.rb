1922deaf3432182f39619b9c5578f400162ee696
Joy Phoebe <joyapisi@gmail.com>
2023-08-03 15:24:56 +0300

    Modify test for users logged in

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
     factory :food do
       name { "Apple" }
-      measurement_unit { "grams" }
+      meaurement_unit { "grams" }
       price { 25.00 }
       quantity { 1 }
       user
--- spec/features/foods_view_spec.rb
+++ spec/features/foods_view_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Foods views', type: :feature do
   let!(:uniqueuser) { FactoryBot.create(:user, name: "uniqueuser", email: "uniqueuser@gmail.com", password: 'uniqueuserpassword', password_confirmation: 'uniqueuserpassword') }
-  let!(:uniquefood) { FactoryBot.create(:food, name: 'Apple', measurement_unit: 'grams', price: 25.00, quantity: 1, user: uniqueuser) }
+  let!(:uniquefood) { FactoryBot.create(:food, name: 'Apple', meaurement_unit: 'grams', price: 25.00, quantity: 1, user: uniqueuser) }
 
   describe 'food#index' do
     it 'should display all created foods' do
+      # Log in the user before visiting the '/foods' page
+      visit login_path
+      fill_in 'Email', with: uniqueuser.email
+      fill_in 'Password', with: 'uniqueuserpassword'
+      click_button 'Log in'
+      
       visit '/foods'
       expect(page).to have_content('Apple')
     end
@@ -13,6 +19,12 @@ RSpec.describe 'Foods views', type: :feature do
 
   describe 'button to form that creates new food' do
     it 'should direct the user to the form that creates new food' do
+      # Log in the user before visiting the '/foods' page
+      visit login_path
+      fill_in 'Email', with: uniqueuser.email
+      fill_in 'Password', with: 'uniqueuserpassword'
+      click_button 'Log in'
+
       visit '/foods'
       click_link 'New Food'
       expect(page).to have_current_path('/foods/new')
