33c4c4a814ddb7297a82fa43acaa6c9b936e7cdc
Joy Phoebe <joyapisi@gmail.com>
2023-08-03 21:42:49 +0300

    Add validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :recipe_food, foreign_key: 'recipe_id'
   has_many :foods, through: :recipe_foods, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
--- spec/features/foods_view_spec.rb
+++ spec/features/foods_view_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe 'Foods views', type: :feature do
-  @uniqueuser = User.create(
-  name: Faker::Name.unique.name,
-  email: Faker::Internet.email, 
-  password: 'uniqueuserpassword', 
-  password_confirmation: 'uniqueuserpassword'
-  )
-  @uniquefood = Food.create(
-    name: 'Apple', 
-    meaurement_unit: 'grams', 
-    price: 25.00, 
-    quantity: 1, 
-    user: @uniqueuser
-    )
+  let(:user) { User.create!(name: "Test User", email: "test@example.com", password: "testpassword") }
+
+  before(:each) do
+    # Manually set the current_user to the signed-in user
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
 
   describe 'food#index' do
-    it 'should display all created foods' do
+    it 'should display all created foods for the current user' do
+      # Create some foods for the user
+      food1 = Food.create(name: 'Apple', meaurement_unit: 'grams', price: 25.00, quantity: 1, user: user)
+      food2 = Food.create(name: 'Banana', meaurement_unit: 'grams', price: 15.00, quantity: 2, user: user)
+
       visit '/foods'
+
+      # Now, test the content on the page
       expect(page).to have_content('Apple')
+      expect(page).to have_content('Banana')
     end
   end
 
@@ -27,7 +27,7 @@ RSpec.describe 'Foods views', type: :feature do
     it 'should direct the user to the form that creates new food' do
       visit '/foods'
       click_link 'New Food'
-      expect(page).to have_current_path('/foods/new')
+      expect(page).to have_current_path(new_food_path)
     end
   end
 end
