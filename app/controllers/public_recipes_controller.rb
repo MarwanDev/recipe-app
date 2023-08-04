79e57b371566fcbf1e156b39d033ebae6414dd44
Joy Phoebe <joyapisi@gmail.com>
2023-08-05 00:47:50 +0300

    Add actions to count total price and total food items for public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,11 @@
 class PublicRecipesController < ApplicationController
-  def index
+    def index
     @public_recipes = Recipe.includes(:user).where(public: true).order(created_at: :desc)
+
+    @total_prices = {}
+    @public_recipes.each do |recipe|
+      total_price = recipe.recipe_foods.sum { |rf| rf.food.price * rf.quantity }
+      @total_prices[recipe.id] = total_price
+    end
   end
-end
+  end
