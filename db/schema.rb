7226a79a5a311a5594dbcb949d48e630e6f5e812
MarwanDev <marwaneg@aucegypt.edu>
2023-08-04 22:33:32 +0300

    Fix authorization and extra column in recipe foods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_155628) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_192625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,11 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_155628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "food_id", null: false
-    t.bigint "recipes_id"
     t.bigint "recipe_id", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
-    t.index ["recipes_id"], name: "index_recipe_foods_on_recipes_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -70,6 +68,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_155628) do
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
-  add_foreign_key "recipe_foods", "recipes", column: "recipes_id"
   add_foreign_key "recipes", "users"
 end
